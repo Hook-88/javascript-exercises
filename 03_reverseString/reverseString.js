c30c36df85fbad815da26e139cfd2739f14c26e3
Paul Hoekjan <paulhoekjan@gmail.com>
2023-03-27 11:31:52 -0500

    Passed all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  //create an empty string reversedString
+  let reversedString = '';
+  //loop reversed trough the string
+  for (let i = ( string.length - 1); i > -1; i--) {
+    //Add string[i] to reversedString
+    reversedString += string[i];
+  }
+  //Return reversedString
+  return reversedString;
 };
 
 // Do not edit below this line
