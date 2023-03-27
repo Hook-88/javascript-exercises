fa64040f27b02802d50947c78973364740d1fd74
Paul Hoekjan <paulhoekjan@gmail.com>
2023-03-27 11:10:00 -0500

    Modify repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, numOfRepeats) {
+  let newString = '';
+  // check if numOfRepeats is negativ
+  if (numOfRepeats >= 0) {
+    for (let i = 0; i < numOfRepeats; i++) {
+      newString += string;
+    }
+    return newString;
+  } else {
+    return 'ERROR';
+  }  
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
