56063a751eea55473b589e515d49c7817eb455d8
Paul Hoekjan <paulhoekjan@gmail.com>
2023-03-27 16:26:33 -0500

    pased all the tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(numbersArray, ...theArgs) {
+  return numbersArray.filter(
+    num => {
+      return !theArgs.includes(num);
+    }
+  )
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,33 @@
-const sumAll = function() {
+const sumAll = function(firstNum, lastNum) {
+  let allNums;
+  if (firstNum < 0 || lastNum < 0) {
+    return 'ERROR';
+  } else {
+    if (typeof firstNum !== 'number' || typeof lastNum !== 'number') {
+      return 'ERROR';
+    } else {
+      if (firstNum < lastNum) {
+        allNums = createArrayFromNumbersSmallToLarge(firstNum, lastNum);
+      } else {
+        allNums = createArrayFromNumbersSmallToLarge(lastNum, firstNum);
+      }
+      //reduce total + num
+      return allNums.reduce((acc, curr) => {
+        return acc + curr;
+      },0);
+    }
+  }
 
+  
 };
+
+const createArrayFromNumbersSmallToLarge = (small, large) => {
+  const allNums = [];
+  for (let i = small; i <= large; i++) {
+    allNums.push(i);
+  }
+  return allNums;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
