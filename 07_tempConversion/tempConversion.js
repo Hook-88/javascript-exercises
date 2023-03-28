b9cf276a1ca8e14ea3f02221d077bfba628e172c
Paul Hoekjan <paulhoekjan@gmail.com>
2023-03-28 12:18:07 -0500

    passed tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInF) {
+  // (F - 32) * (5/9)
+  let raw = (tempInF - 32) * (5/9); 
+  return roundNumToTwoDecimals(raw) ;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInC) {
+  // C * (9/5) + 32
+  let raw = (tempInC * (9/5) + 32);
+  return roundNumToTwoDecimals(raw);
 };
+
+const roundNumToTwoDecimals = num => {
+  return Math.round(num * 10) / 10;
+}
 
 // Do not edit below this line
 module.exports = {
