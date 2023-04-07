08422bc27027232efbaa1964da093c56095b741b
Paul Hoekjan <paulhoekjan@gmail.com>
2023-04-07 10:26:26 -0500

    just missing the fractal, need to look up what fractal is

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,21 +1,25 @@
-const add = function() {
-	
+const add = function(numOne, numTwo) {
+	return numOne + numTwo;
 };
 
-const subtract = function() {
-	
+const subtract = function(numOne, numTwo) {
+	return numOne - numTwo;
 };
 
-const sum = function() {
-	
+const sum = function(numberArr) {
+	return numberArr.reduce( (a, b) => {
+    return a + b;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numberArr) {
+  return numberArr.reduce(function(a, b) {
+    return a * b;
+  }, 1)
 };
 
-const power = function() {
-	
+const power = function(numOne, numTwo) {
+	return numOne ** numTwo;
 };
 
 const factorial = function() {
