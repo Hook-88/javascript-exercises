419d451d2053267db8cd58c5e44b5839e0e9e06b
Paul Hoekjan <paulhoekjan@gmail.com>
2023-04-07 10:49:08 -0500

    Add factorial

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -22,8 +22,14 @@ const power = function(numOne, numTwo) {
 	return numOne ** numTwo;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n === 0) {
+    return 1;
+  } else if (n < 0) {
+    return 0;
+  } else {
+    return n * factorial(n-1);
+  }
 };
 
 // Do not edit below this line
