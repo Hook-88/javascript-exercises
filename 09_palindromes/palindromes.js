02f880bb4313083d8a0ef00a620ee0562390d3a1
Paul Hoekjan <paulhoekjan@gmail.com>
2023-04-11 09:32:35 -0500

    Add comments

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,7 +1,10 @@
 const palindromes = function (string) {
+  //create an array and set it to the filterd string
   let originalStringArray = filterString(string);
+  //create an array which is the opposite of the original string array
   let reversedArray = reverseArray(originalStringArray);
-return arrayToString(originalStringArray) === arrayToString(reversedArray);
+  //check if original and reverse are the same
+  return arrayToString(originalStringArray) === arrayToString(reversedArray);
 };
 
 // Do not edit below this line
@@ -9,13 +12,16 @@ module.exports = palindromes;
 
 
 function filterString(string) {
-  let stringArray = string.split('');
-  let filteredStringArray = stringArray.filter(char => {
-    return char.toUpperCase() !== char.toLowerCase();
-  }).map(char => {
+  // spilt string in array
+  return string.split('')
+    // filter out al the non letter chars to deal with punctuation and multiple words
+    .filter(char => {
+      return char.toUpperCase() !== char.toLowerCase();
+    })
+    // convert to uppercase to deal with uppercase letters
+    .map(char => {
     return char.toUpperCase();
-  })
-  return filteredStringArray;
+    });
 }
 
 function reverseArray(array) {
